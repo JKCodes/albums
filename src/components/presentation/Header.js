d8d9691d65297908410774224feceb6b78721e85
Joe Kim <josephkim0224@gmail.com>
2018-01-02 14:34:46 -0500

    implement Header Component

--- src/components/presentation/Header.js
+++ src/components/presentation/Header.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
+  const { header, view } = styles;
+
   return (
-
-    <Text>Albums</Text>
- 
+    <View style={view}>
+      <Text style={header}>{props.headerText}</Text>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 20
+  },
+  view: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { widht: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  }
+});
 
 export default Header;
